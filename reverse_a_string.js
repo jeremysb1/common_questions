bdbc47dd1d5d2383b31f9145315a50a9e865b9cb
Jeremy Hall <21281011+jeremysb1@users.noreply.github.com>
2021-01-28 13:43:56 -0500

    refactored solution by adding destructuring
    solution includes iteration, built-in methods, ES6 arrow functions, and destructuring assignment

--- reverse_a_string.js
+++ reverse_a_string.js
@@ -5,15 +5,15 @@
 // this is an iterative way of solving the problem
 
 function reverseString(str) {
-	if (!str || str.length < 2 || typeof str !== 'string') {
-		return 'enter a better input';
-	}
-	const backwards = [];
-	const totalItems = str.length - 1;
-	for (let i = totalItems; i >= 0; i--) {
-		backwards.push(str[i]);
-	}
-	return backwards.join('');
+  if (!str || str.length < 2 || typeof str !== 'string') {
+    return 'improve your input, yo';
+  }
+  const backwards = [];
+  const totalItems = str.length - 1;
+  for (let i = totalItems; i >= 0; i--) {
+    backwards.push(str[i]);
+  }
+  return backwards.join('');
 }
 
 //using built in methods
@@ -22,6 +22,10 @@ function reverseString2(str) {
   return str.split('').reverse().join('');
 }
 
-//using ES6 arrow functions
+// using ES6 arrow functions
 
 const reverse3 = str => str.split('').reverse().join('');
+
+//using destructuring assignment
+
+const reverse4 = str => [...str].reverse().join('');
